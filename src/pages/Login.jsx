9cb0be05d07be57833f5ef0684e54aea8000803f
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-04-26 19:07:06 -0300

    R:02 - 04

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,34 @@ import React from 'react';
 function Login() {
   return (
     <>
-      Login
+      <h1>
+        Login
+      </h1>
+
+      <label
+        htmlFor="email"
+      >
+        Email
+        <input
+          id="email"
+          data-testid="email-input"
+        />
+      </label>
+      <label
+        htmlFor="password"
+      >
+        Senha
+        <input
+          id="password"
+          data-testid="password-input"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
     </>
   );
 }
