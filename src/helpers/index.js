8881a1dc9b6b8e383100f5d98a929a41fcc2419d
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-04-29 16:06:44 -0300

    Barra de busca: requisito 18

---
+++ src/helpers/index.js
@@ -0,0 +1,9 @@
+const verifyMeals = (meals, drinks, searchInputValue) => {
+  const filterMeals = meals.find((meal) => meal === searchInputValue);
+  const filterDrinks = drinks.find((drink) => drink === searchInputValue);
+  if (filterDrinks === undefined || filterMeals === undefined) {
+    global.alert('Sorry, we haven\'t found any recipes for these filters.');
+  }
+};
+
+export default verifyMeals;
