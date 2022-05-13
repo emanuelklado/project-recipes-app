8706eca831cd988f1b5794cbef0f725025065ac2
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-05-13 12:21:54 -0300

    Profile Style

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeaderNoSearch from '../components/HeaderNoSearch';
 import Footer from '../components/Footer';
 import ProfileSpecs from '../components/ProfileSpecs';
+import '../style/profile.css';
 
 function Profile() {
   return (
